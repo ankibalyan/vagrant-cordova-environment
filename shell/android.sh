bb0e29fdc2c1ebb71c1f3efd6841b00bc29ccef9
Ankit Balyan <sf.ankit@gmail.com>
2015-08-11 02:10:28 +0530

    Android rules

--- shell/android.sh
+++ shell/android.sh
@@ -13,3 +13,24 @@ echo "ANDROID_HOME=/opt/android-sdk-linux" >> /home/vagrant/.bashrc
 echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /home/vagrant/.bashrc
 echo "PATH=\$PATH:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
 
+sudo cat > /etc/udev/rules.d/51-android.rules <<EOF
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0502", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="12d1", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="1004", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="22b8", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04e8", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fce", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0489", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", SYMLINK+="android_adb", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04e8", MODE="0666", GROUP="plugdev"
+EOF
