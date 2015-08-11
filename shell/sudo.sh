aa3011b5dc61af329e22f9c1b2a4b241559e3d4d
Ankit Balyan <sf.ankit@gmail.com>
2015-08-11 16:55:17 +0530

    Correction

--- shell/sudo.sh
+++ shell/sudo.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install openjdk-7-jre
 apt-get install -y npm git ant expect
 
 sudo apt-add-repository ppa:cordova-ubuntu/ppa
+apt-get update
 sudo apt-get install cordova-cli
 
-apt-get update apt-get install -f
+apt-get update && apt-get install -f
