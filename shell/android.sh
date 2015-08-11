fae00a4fc3f36d367d1e79591c7fd2b0ddedaa0a
Ankit Balyan <sf.ankit@gmail.com>
2015-08-11 17:12:56 +0530

    android skd url updated

--- shell/android.sh
+++ shell/android.sh
@@ -4,7 +4,7 @@ SDK_NAME=android-sdk_r24-linux.tgz
 SDK_URL=http://dl.google.com/android/$SDK_NAME
 
 cd /tmp
-curl -0 SDK_URL
+curl -0 $SDK_URL
 sudo tar -xzvf $SDK_NAME && mv -rf android-sdk-linux /opt/
 sudo chown -Rf vagrant /opt/android-sdk-linux
 sudo chmod -Rf 755 /opt/android-sdk-linux
